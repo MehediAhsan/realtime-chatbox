bc9ad69b8d1ae8138984d6605fae3c119870f9e3
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-07-06 00:37:08 +0600

    use moment js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+// external imports
 const express = require("express");
+const http = require("http");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const moment = require("moment");
+
+// internal imports
 const loginRouter = require("./router/loginRouter");
 const usersRouter = require("./router/usersRouter");
 const inboxRouter = require("./router/inboxRouter");
@@ -14,7 +19,15 @@ const {
 } = require("./middlewares/common/errorHandler");
 
 const app = express();
+const server = http.createServer(app);
 dotenv.config();
+
+// socket creation
+const io = require("socket.io")(server);
+global.io = io;
+
+// set comment as app locals
+app.locals.moment = moment;
 
 // database connection
 mongoose
@@ -43,12 +56,12 @@ app.use("/", loginRouter);
 app.use("/users", usersRouter);
 app.use("/inbox", inboxRouter);
 
-// 404 not found error handler
+// 404 not found handler
 app.use(notFoundHandler);
 
-// error handler
+// common error handler
 app.use(errorHandler);
 
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
   console.log(`app listening to port ${process.env.PORT}`);
 });
